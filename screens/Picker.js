6745a8771e50c8f723202969f65f671bade6bd3f
Tatiana Gripasova <botnar.tatiana@gmail.com>
2019-12-03 10:14:40 -0500

    Add date time picker for events

--- screens/Picker.js
+++ screens/Picker.js
@@ -1,14 +1,21 @@
 import React, { useState, useEffect } from "react";
 import { View, StyleSheet, TouchableOpacity } from "react-native";
+import { Button } from "react-native-elements";
 import MapView, { Marker } from "react-native-maps";
 import Input from "../components/Input";
 import ConditionalView from "../components/ConditionalView";
 import Autocomplete from "../screens/Autocomplete";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+import moment from "moment";
 
 const Picker = props => {
     const [fromLocation, setFromLocation] = useState(props.location);
     const [toLocation, setToLocation] = useState({});
     const [autocomplete, setAutocomplete] = useState(false);
+    const [timePicker, setTimePicker] = useState(false);
+    const [datePicker, setDatePicker] = useState(false);
+    const [eventTime, setEventTime] = useState(null);
+    const [eventDate, setEventDate] = useState(null);
 
     // TODO: Don't mutate state, remove once main page is ready.
     useEffect(() => {
@@ -23,6 +30,34 @@ const Picker = props => {
     const selectAddress = (aa) => {
         console.log('>>>>>>>>>>>>>>>', aa);
         setAutocomplete(false);
+    }
+
+    const showTimePicker = () => {
+        setTimePicker(true);
+      };
+    
+    const hideTimePicker = () => {
+        setTimePicker(false);
+      };
+    
+    const handleTimeConfirm = time => {
+        const eventTime = moment(time).format("LT");
+        setEventTime(eventTime);
+        hideTimePicker();
+    };
+
+    const showDatePicker = () => {
+        setDatePicker(true);
+    };
+
+    const hideDatePicker = () => {
+        setDatePicker(false)
+    };
+
+    const handleDateConfirm = date => {
+        const eventDate = moment(date).format("MMM Do YY");
+        setEventDate(eventDate);
+        hideDatePicker();
     }
 
     return (
@@ -56,6 +91,40 @@ const Picker = props => {
                     pointerEvents={"none"}
                 />
             </TouchableOpacity>
+            <View>
+                <Button 
+                    title={eventTime ? eventTime : "Pick Time"} 
+                    type="clear"
+                    onPress={showTimePicker} 
+                />
+                <DateTimePickerModal
+                    isVisible={timePicker}
+                    mode={"time"}
+                    onConfirm={handleTimeConfirm}
+                    onCancel={hideTimePicker}
+                />
+            </View>
+            <View>
+                <Button 
+                    title={eventDate ? eventDate : "Once"} 
+                    type="clear"
+                    onPress={showDatePicker} 
+                />
+                <DateTimePickerModal
+                    isVisible={datePicker}
+                    mode={"date"}
+                    onConfirm={handleDateConfirm}
+                    onCancel={hideDatePicker}
+                />
+            </View>
+            <View>
+                <Button 
+                    title={eventDate ? eventDate : "Repeat"} 
+                    type="clear"
+                    onPress={showDatePicker} 
+                />
+                
+            </View>
         </View>
         </ConditionalView>
     )
